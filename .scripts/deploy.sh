1fef26f1b26e770d535fab885160f695edf536d7
bmartins <bsamartins@gmail.com>
2018-01-20 16:24:27 +0000

    no message

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,14 +1,11 @@
 set -e
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-
 bundle exec jekyll build
 
 git add --all -f _site/
 git commit -m 'Updating.'
 
 git remote add origin-pages "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
-git push --quiet --set-upstream origin-pages ${TRAVIS_BRANCH}
+git push --quiet --set-upstream origin-pages master
 
 echo "Pushed updated branch to GitHub Pages"
