d09627a51133ff39e2b4fb11fe48ca4af9d7ebe5
bmartins <bsamartins@gmail.com>
2018-01-20 12:06:59 +0000

    Forgot domain

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,6 +3,6 @@ bundle exec jekyll build
 git add --all -f _site/
 git commit -m 'Updating.'
 
-git push "https://${GH_TOKEN}@${TRAVIS_REPO_SLUG}"
+git push "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
 
 echo "Pushed updated branch to GitHub Pages"
