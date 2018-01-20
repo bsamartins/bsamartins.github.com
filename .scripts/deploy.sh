bd58addf81be50b188573af67ce3007a8038e1f6
bmartins <bsamartins@gmail.com>
2018-01-20 12:16:42 +0000

    no message

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,6 +3,6 @@ bundle exec jekyll build
 git add --all -f _site/
 git commit -m 'Updating.'
 
-git push --set-upstream=${TRAVIS_BRANCH} "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
+git push --set-upstream ${TRAVIS_BRANCH} "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}"
 
 echo "Pushed updated branch to GitHub Pages"
